d54140d70790809222f08578c78ecb905f683c11
jerry-lllman <jerry_lllman@163.com>
2022-07-13 19:15:49 +0800

    feat: 实现函数的Field

--- src/components/my-rc-field-form/Field.jsx
+++ src/components/my-rc-field-form/Field.jsx
@@ -1,40 +1,79 @@
-import React, { Component, useEffect } from "react"
+import React, { Component, useContext, useLayoutEffect, useState } from "react"
 import FieldContext, { HOOK_MARK } from "./FieldContext"
 
-export default class Field extends React.Component {
-	static contextType = FieldContext
+// export default class Field extends React.Component {
+// 	static contextType = FieldContext
 
-	componentDidMount() {
-		const { registerField } = this.context.getInternalHooks(HOOK_MARK)
-		this.unregister = registerField(this)
-	}
+// 	componentDidMount() {
+// 		const { registerField } = this.context.getInternalHooks(HOOK_MARK)
+// 		this.unregister = registerField(this)
+// 	}
 
-	componentWillUnmount() {
-		this.unregister()
-	}
+// 	componentWillUnmount() {
+// 		this.unregister()
+// 	}
 
 
-	onStoreChange = () => {
-		this.forceUpdate()
-	}
+// 	onStoreChange = () => {
+// 		this.forceUpdate()
+// 	}
 
-	getControlled = () => {
+// 	getControlled = () => {
+// 		return {
+// 			value: this.context?.getFieldValue(this.props.name),
+// 			onChange: (e) => {
+// 				const value = e.target.value
+// 				this.context?.setFieldsValue({
+// 					[this.props.name]: value
+// 				})
+// 			}
+// 		}
+// 	}
+
+// 	render() {
+// 		const returnChildNode = React.cloneElement(this.props.children, this.getControlled())
+
+// 		return <div>
+// 			{returnChildNode}
+// 		</div>
+// 	}
+// }
+
+
+export default function Field(props) {
+
+	const { name, children } = props
+
+	const { getInternalHooks, getFieldValue, setFieldsValue } = useContext(FieldContext)
+
+	const [, forceUpdate] = useState({})
+
+	// 使用 useEffect 会导致订阅的太晚，业务使用 this.formRef.current.setFieldsValue({ username: "jerry" })
+	// 无法设置到值，所以需要使用 useLayoutEffect
+
+	useLayoutEffect(() => {
+		const { registerField } = getInternalHooks(HOOK_MARK)
+		const unregister = registerField({ props, onStoreChange: () => forceUpdate({}) })
+
+		return () => {
+			unregister()
+		}
+	}, [])
+
+	const getControlled = () => {
 		return {
-			value: this.context?.getFieldValue(this.props.name),
+			value: getFieldValue(name),
 			onChange: (e) => {
 				const value = e.target.value
-				this.context?.setFieldsValue({
-					[this.props.name]: value
+				setFieldsValue({
+					[name]: value
 				})
 			}
 		}
 	}
 
-	render() {
-		const returnChildNode = React.cloneElement(this.props.children, this.getControlled())
-
-		return <div>
-			{returnChildNode}
-		</div>
-	}
+	const returnChildNode = React.cloneElement(children, getControlled())
+	return <div>
+		{returnChildNode}
+	</div>
 }
